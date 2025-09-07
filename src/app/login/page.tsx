7d680dfac839f1c98fbab5a5fd9273d50e861c79
Thuan Silva <thuansilva14@gmail.com>
2025-09-07 01:57:51 -0400

    feat: create route in the project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import '../../presentation/styles/global.scss';
+
+import LoginComponent from '~/presentation/pages/login/login';
+
+export default function Login() {
+  return (
+    // <main>
+    <LoginComponent />
+    // </main>
+  );
+}
